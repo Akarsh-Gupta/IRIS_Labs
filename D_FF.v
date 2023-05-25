278e815a9a54724e1d08727ed843ac016b63a5f0
Akarsh-Gupta <95587589+Akarsh-Gupta@users.noreply.github.com>
2023-05-26 03:56:40 +0530

    Update D_FF.v

--- D_FF.v
+++ D_FF.v
@@ -1,8 +1,17 @@
-module HelloWorld;
+module DFF_with_Reset(
+  input wire clk,
+  input wire reset,
+  input wire d,
+  output reg q
+);
 
-	initial begin
-		$display("Hello World");
-		$finish;
-	end
+  always @(posedge clk or negedge reset) begin
+    if (reset == 1'b0) begin
+      q <= 1'b0;   // Reset the output to 0 when reset is asserted
+    end
+    else begin
+      q <= d;      // Update the output with the input value when reset is de-asserted
+    end
+  end
 
 endmodule
